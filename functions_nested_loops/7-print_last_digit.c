54d6d7cc43452c5127b7b431e2351858cfe6bde3
Junius.Whitaker <root@8d5580a19f8e>
2023-09-22 20:16:57 -0700

    prints the last digit of a number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <main.h>
 
 /**
  * print_last_digit - Prints the last digit of an integer
@@ -18,8 +18,24 @@ int print_last_digit(int x)
 	}
 	else
 	{
-		last_digit = -x % 10;
+		if (x == INT_MIN)
+		{
+			last_digit = 8; // Last digit of INT_MIN is 8
+		}
+		else
+		{
+			last_digit = -x % 10;
+		}
+
 		_putchar('0' + last_digit);
 		return (last_digit);
 	}
 }
+In the updated code, we explicitly handle the case when x is INT_MIN and set the last_digit accordingly. The last digit of INT_MIN is 8, so we set last_digit to 8 in this case.
+
+Now, calling print_last_digit(INT_MIN) should correctly print the last digit, which is 8, and return 8.
+
+
+
+
+
